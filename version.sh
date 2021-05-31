d291c8df8cbb443a8d986a6c2b2e53bf57964269
Mert ÇELEN <mcelen94@gmail.com>
2021-05-31 16:11:22 +0000

    New data added.

--- version.sh
+++ version.sh
@@ -2,4 +2,4 @@
 
 VERSION=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
 ROOT=$(git rev-parse --show-toplevel)
-sed -i "s/RANDOM_VER/$VERSION/g" $ROOT/index.html
+sed -i "s/ver=/ver=$VERSION/g" $ROOT/index.html
