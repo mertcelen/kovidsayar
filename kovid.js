a6040672b62c9512eabf14774c0b6d6a30609a92
Mert ÇELEN <mcelen94@gmail.com>
2021-07-07 21:21:08 +0000

    UI simplified.

--- kovid.js
+++ kovid.js
@@ -3,6 +3,10 @@
  let sourceKey = dataKeys[dataKeys.length - 1];
  let lastType = 2;
  let colorType = 2;
+ let yellowCount = 0;
+ let blueCount = 0;
+ let redCount = 0;
+ let orangeCount = 0;
  svgturkiyeharitasi();
  calculate(2);
  function calculate(type = null){
@@ -37,28 +41,48 @@
             Array.from(element.children).forEach(path => path.style.fill = cityColor);
         });
     });
+    // setCounts();
  }
 
  function riskColor(data){
     if (data < 10){
+        blueCount++;
         return '#0d6efd';
     }else if(data < 35){
+        yellowCount++;
         return '#f0e513';
     }else if(data < 100){
+        orangeCount++;
         return '#f8931f';
     }else{
+        redCount++;
         return '#df1a23';
     }
  }
 
+ function setCounts(){
+     document.getElementById("redCount").innerHTML = redCount;
+     document.getElementById("blueCount").innerHTML = blueCount;
+     document.getElementById("orangeCount").innerHTML = orangeCount;
+     document.getElementById("yellowCount").innerHTML = yellowCount;
+     yellowCount = 0;
+     redCount = 0;
+     blueCount = 0;
+     orangeCount = 0;
+ }
+
  function riskColor2(data){
     if (data < 20){
+        blueCount++;
         return '#0d6efd';
     }else if(data < 50){
+        yellowCount++;
         return '#f0e513';
     }else if(data < 100){
+        orangeCount++;
         return '#f8931f';
     }else{
+        redCount++;
         return '#df1a23';
     }
  }
@@ -110,4 +134,61 @@ window.onload = function(){
         selector.add(option);
     }
     selector.lastChild.selected = 'selected';
+}
+
+
+function svgturkiyeharitasi() {
+    const element = document.querySelector('#svg-turkiye-haritasi');
+    const info = document.querySelector('.il-isimleri');
+  
+    element.addEventListener(
+      'mouseover',
+      function (event) {
+        if (event.target.tagName === 'path') {
+          let current = event.target.parentNode.getAttribute('data-iladi');
+          toplam = Math.floor((nufus[current] < 100000 ? 100000 : nufus[current]) / 100000 * parseFloat(kovid[sourceKey][current]))
+          toplamEski = Math.floor((nufus[current] < 100000 ? 100000 : nufus[current]) / 100000 * parseFloat(kovid[findPreviousKey(sourceKey)][current]))
+          gunluk = Math.round(toplam / 7);
+          gunlukEski = Math.round(toplamEski / 7);
+          yuzbinde = parseFloat(kovid[sourceKey][current])
+          yuzbindeEski = parseFloat(kovid[findPreviousKey(sourceKey)][current])
+          info.innerHTML = [
+            '<div><b>',
+            event.target.parentNode.getAttribute('data-iladi') + "</b><br><br>" + aciklamalar[sourceKey] + "<br>",
+            'Yüzbinde : ' + yuzbinde + '<br>',
+            'Toplam : ' + toplam + '<br><br>' + aciklamalar[findPreviousKey(sourceKey)] + '<br>',
+            'Yüzbinde : ' + yuzbindeEski + '<br>',
+            'Toplam : ' + toplamEski,
+            '</div>'
+          ].join('');
+        }
+      }
+    );
+  
+    element.addEventListener(
+      'mousemove',
+      function (event) {
+        info.style.top = event.pageY + 25 + 'px';
+        info.style.left = event.pageX + 'px';
+      }
+    );
+  
+    element.addEventListener(
+      'mouseout',
+      function (event) {
+        info.innerHTML = '';
+      }
+    );
+  }
+
+
+function findPreviousKey(currentKey){
+    let previous = null;
+    for(let element of Object.keys(kovid)) {
+        if(element == currentKey){
+            return previous;
+        }
+        previous = element;
+    }
+    return currentKey;
 }
